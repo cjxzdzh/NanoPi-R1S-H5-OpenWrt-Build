b83d5194d81a8995943949ddabaa0bdd79180e9d
cjxzdzh <40509121+cjxzdzh@users.noreply.github.com>
2020-02-10 21:08:51 +0800

    Update onekey.sh

--- onekey.sh
+++ onekey.sh
@@ -24,8 +24,8 @@ git clone https://github.com/coolsnowwolf/lede
 cd friendlywrt
 cp -r ../lede/package/lean package/
 #删除openwrt的更新下载源，改为lede的源
-rm -f ./feeds.conf.default
-wget https://raw.githubusercontent.com/coolsnowwolf/lede/master/feeds.conf.default
+sed -i 's/https:\/\/git.openwrt.org\/project\/luci.git^039ef1f4deba725d3591b159bbc9569885d68131/https:\/\/github.com\/coolsnowwolf\/luci/' feeds.conf.default
+sed -i 's/https:\/\/git.openwrt.org\/feed\/packages.git^00803ffc91e80b16e9c1603ff32106d42e255923/https:\/\/github.com\/coolsnowwolf\/packages/' feeds.conf.default          
 cd package
 #这里装了个自定义插件koolproxy
 git clone https://github.com/Baozisoftware/luci-app-koolproxy
@@ -34,10 +34,15 @@ cd include
 #这里把目标mk里的dnsmasq替换成了dnsmasq-full default-settings luci。
 sed -i 's/dnsmasq /dnsmasq-full default-settings luci /' target.mk
 cd ..
+./scripts/feeds update -a
+./scripts/feeds install -a
 rm -f ./.config*
 #使用skytotwo的r1s-h5-config作为默认的编译config，这里可以使用自己的config代替
 wget https://raw.githubusercontent.com/skytotwo/NanoPi-R1S-Build-By-Actions/master/r1s-h5-config
 cp r1s-h5-config .config
+make download -j8
+find dl -size -1024c -exec ls -l {} \;
+find dl -size -1024c -exec rm -f {} \;
 cd ..
 cd scripts
 #替换build.sh和mk-friendlywrt.sh，主要避免一些奇怪的问题，略微修改了一点。
