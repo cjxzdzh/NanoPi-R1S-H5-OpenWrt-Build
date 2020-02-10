2b5445acc1830c4f6883e3a631de9acc3b754388
cjxzdzh <40509121+cjxzdzh@users.noreply.github.com>
2020-02-10 18:38:57 +0800

    Update onekey.sh

--- onekey.sh
+++ onekey.sh
@@ -1,7 +1,7 @@
 rm -f build.sh
-wget https://raw.githubusercontent.com/cjxzdzh/-NanoPi-R1S-H5-Build/master/build.sh
+wget -O https://raw.githubusercontent.com/cjxzdzh/-NanoPi-R1S-H5-Build/master/build.sh
 rm -f mk-friendlywrt.sh
-wget https://raw.githubusercontent.com/cjxzdzh/-NanoPi-R1S-H5-Build/master/mk-friendlywrt.sh
+wget -O https://raw.githubusercontent.com/cjxzdzh/-NanoPi-R1S-H5-Build/master/mk-friendlywrt.sh
 cd ..
 sudo ./build.sh nanopi_r1s.mk
 cd ..
