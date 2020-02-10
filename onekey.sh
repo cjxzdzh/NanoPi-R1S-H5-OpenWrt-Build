e42f593f871b04058c884ac623e78f863cfc7daa
cjxzdzh <40509121+cjxzdzh@users.noreply.github.com>
2020-02-10 17:55:46 +0800

    Update onekey.sh

--- onekey.sh
+++ onekey.sh
@@ -1,39 +1,3 @@
-#!/bin/bash
-sudo apt-get update
-wget -O - https://raw.githubusercontent.com/friendlyarm/build-env-on-ubuntu-bionic/master/install.sh | bash
-git clone https://github.com/friendlyarm/repo
-sudo cp repo/repo /usr/bin/
-mkdir friendlywrt-h5
-cd friendlywrt-h5
-repo init -u https://github.com/friendlyarm/friendlywrt_manifests -b master -m h5.xml --repo-url=https://github.com/friendlyarm/repo  --no-clone-bundle
-#使用skytotwo的h5.xml
-cd .repo/manifests
-rm -f ./h5.xml
-wget https://raw.githubusercontent.com/skytotwo/NanoPi-R1S-Build-By-Actions/master/source_xml/h5_19.07.xml
-cp h5_19.07.xml h5.xml
-rm -rf ./h5_19.07.xml
-repo sync -c  --no-clone-bundle
-cd ..
-cd ..
-git clone https://github.com/coolsnowwolf/lede
-cd friendlywrt
-cp -r ../lede/package/lean package/
-sed -i 's/https:\/\/git.openwrt.org\/project\/luci.git^039ef1f4deba725d3591b159bbc9569885d68131/https:\/\/github.com\/coolsnowwolf\/luci/' feeds.conf.default
-sed -i 's/https:\/\/git.openwrt.org\/feed\/packages.git^00803ffc91e80b16e9c1603ff32106d42e255923/https:\/\/github.com\/coolsnowwolf\/packages/' feeds.conf.default 
-cd package
-#这里装了个自定义插件koolproxy
-git clone https://github.com/Baozisoftware/luci-app-koolproxy
-cd ..
-cd include
-#这里把目标mk里的dnsmasq替换成了dnsmasq-full default-settings luci。
-sed -i 's/dnsmasq /dnsmasq-full default-settings luci /' target.mk
-cd ..
-rm -f ./.config*
-#使用skytotwo的r1s-h5-config
-wget https://raw.githubusercontent.com/skytotwo/NanoPi-R1S-Build-By-Actions/master/r1s-h5-config
-cp r1s-h5-config .config
-cd ..
-cd scripts
 rm -f mk-friendlywrt.sh
 wget https://raw.githubusercontent.com/cjxzdzh/-NanoPi-R1S-H5-Build/master/mk-friendlywrt.sh
 cd ..
