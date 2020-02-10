9807f8c949bafe04bdb3a83de8c46dab0fcf4f69
cjxzdzh <40509121+cjxzdzh@users.noreply.github.com>
2020-02-10 18:47:43 +0800

    Update onekey.sh

--- onekey.sh
+++ onekey.sh
@@ -2,7 +2,9 @@ rm -f build.sh
 wget https://raw.githubusercontent.com/cjxzdzh/-NanoPi-R1S-H5-Build/master/build.sh
 rm -f mk-friendlywrt.sh
 wget https://raw.githubusercontent.com/cjxzdzh/-NanoPi-R1S-H5-Build/master/mk-friendlywrt.sh
+sudo chmod 777 build.sh
+sudo chmod 777 mk-friendlywrt.sh
 cd ..
-sudo ./build.sh nanopi_r1s.mk
+./build.sh nanopi_r1s.mk
 cd ..
 find friendlywrt-h5/out/ -name "FriendlyWrt_*img*" | xargs -i zip -r {}.zip {}
